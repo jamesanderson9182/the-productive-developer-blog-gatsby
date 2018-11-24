290a33d531e023c9cd26076d64ca53f17a4d5ed2
James Anderson <james.anderson9182@gmail.com>
2018-11-24 14:08:33 +0000

    personalise to the productive developer brand a bit

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -5,7 +5,7 @@ import 'typeface-montserrat'
 import 'typeface-merriweather'
 
 import profilePic from './profile-pic.jpg'
-import { rhythm } from '../utils/typography'
+import {rhythm} from '../utils/typography'
 
 class Bio extends React.Component {
   render() {
@@ -18,7 +18,7 @@ class Bio extends React.Component {
       >
         <img
           src={profilePic}
-          alt={`Kyle Mathews`}
+          alt={`James Anderson`}
           style={{
             marginRight: rhythm(1 / 2),
             marginBottom: 0,
@@ -26,13 +26,7 @@ class Bio extends React.Component {
             height: rhythm(2),
           }}
         />
-        <p>
-          Written by <strong>Kyle Mathews</strong> who lives and works in San
-          Francisco building useful things.{' '}
-          <a href="https://twitter.com/kylemathews">
-            You should follow him on Twitter
-          </a>
-        </p>
+        <p> Author <strong>James Anderson</strong></p>
       </div>
     )
   }
